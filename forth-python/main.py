3ea6d9526f2103e07cc92387cb9fd614b31a79bf
Ivan Kavalerov <ivan@kavalerov.net>
2022-07-07 22:31:01 +0100

    Simple REPL set up

--- forth-python/main.py
+++ forth-python/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def process_line(line: str):
+    return "You said: " + line
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        uinput = sys.stdin.readline()
+        output = process_line(uinput)
+        print(output)
